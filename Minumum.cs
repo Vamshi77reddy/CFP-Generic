09b57752681192ba9234d2d98cd359737725a338
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-09-01 11:23:47 +0530

    Generic problems

---
+++ Minumum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxandMinUsingGenerics
+{
+    internal class Minumum
+    {
+        public void FindMinimum(int number1, int number2, int number3)
+        {
+            int minimum = number1;
+            if (number2 < minimum)
+            {
+                minimum = number2;
+            }
+            if (number3 < minimum)
+            {
+                minimum = number3;
+            }
+            Console.WriteLine($"The Minimum of the given three numbers {number1}, {number2} and {number3} is: {minimum}");
+        }
+
+        public void FindMinimum2(double number1, double number2, double number3)
+        {
+            double minimum = number1;
+            if (number2 < minimum)
+            {
+                minimum = number2;
+            }
+            if (number3 < minimum)
+            {
+                minimum = number3;
+            }
+            Console.WriteLine($"The Minimum of the given three numbers {number1}, {number2} and {number3} is: {minimum}");
+        }
+
+        public void FindMinimum3(string string1, string string2, string string3)
+        {
+            string minimum = string1;
+            if (string2.CompareTo(minimum) < 0)
+            {
+                minimum = string2;
+            }
+            if (string3.CompareTo(minimum) < 0)
+            {
+                minimum = string3;
+            }
+            Console.WriteLine($"The Minimum of the given three strings {string1}, {string2} and {string3} is: {minimum}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MaxandMinUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Minumum F1 = new Minumum();
+             F1.FindMinimum(21,34,78);
+             F1.FindMinimum2(23.4,43.6,12.5);
+             F1.FindMinimum3("vamshi","sai","rahul");*/
+
+            MinumunUsingGenerics minumum = new MinumunUsingGenerics();
+            minumum.FindMinimum(21, 34, 78);
+            minumum.FindMinimum(23.4, 43.6, 12.5);
+            minumum.FindMinimum("vamshi", "sai", "rohith");
+        }
+    }
+}
